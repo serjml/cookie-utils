c524fc84d4154f732905700efcdef68641f98d58
Serj Lyakhov <serjml@list.ru>
2025-08-25 08:35:00 +0300

    feat: get set cookie function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const setCookie = (name, value) => {
   document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)};`;
 };
+
+const getCookie = (name) => {
+  const cookies = document.cookie.split('; ');
+  
+  for (const entryString of cookies) {
+    const [entryName, entryValue] = entryString.split('=');
+    
+    if (decodeURIComponent(entryName) === name) {
+      return entryValue;
+    }
+  }
+};
